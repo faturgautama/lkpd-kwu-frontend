1bcbb731715281e310ae3ebb10f56bdf4b391936
Fatur Gautama <fatur@paycombat.com>
2025-01-25 21:17:44 +0700

    feat: beranda page

--- src/app/pages/beranda/beranda.component.ts
+++ src/app/pages/beranda/beranda.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Route, Router } from '@angular/router';
 import { MainComponent } from 'src/app/components/layout/main/main.component';
 
 @Component({
@@ -12,4 +13,50 @@ import { MainComponent } from 'src/app/components/layout/main/main.component';
 })
 export class BerandaComponent {
 
+    Menu = [
+        {
+            id: 'materi',
+            url: '/materi',
+            title: 'Materi',
+            icon: 'pi pi-file',
+        },
+        {
+            id: 'kuis',
+            url: '/kuis',
+            title: 'Kuis',
+            icon: 'pi pi-pen-to-square',
+        },
+        {
+            id: 'proyek',
+            url: '/proyek',
+            title: 'Proyek',
+            icon: 'pi pi-warehouse',
+        },
+        {
+            id: 'nilai',
+            url: '/nilai',
+            title: 'Nilai',
+            icon: 'pi pi-trophy',
+        },
+        {
+            id: 'referensi',
+            url: '/referensi',
+            title: 'Referensi',
+            icon: 'pi pi-file-check',
+        },
+        {
+            id: 'profile',
+            url: '/profile',
+            title: 'Profile',
+            icon: 'pi pi-user-edit',
+        },
+    ];
+
+    constructor(
+        private _router: Router,
+    ) { }
+
+    handleNavigateMenu(url: string) {
+        this._router.navigateByUrl(url);
+    }
 }
